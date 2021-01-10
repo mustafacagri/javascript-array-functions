66b1c6ba44ad3e6814c90c20c7fbc1ef0810137b
Mustafa Çağrı Güven <mguven@sabanciuniv.edu>
2021-01-10 17:31:01 +0300

    sonucYok
    magic string

--- find.js
+++ find.js
@@ -1,5 +1,7 @@
 const kisiler = require('./kisiler.json')
 
+// let sonucYok = "Sonuç Bulunamadı"
+
 // 1-) Yaşı 83 olan kişiyi bulalım.
 let yas83 = kisiler.find( kisi => kisi.yas === 83 )
 console.log(yas83)
@@ -17,3 +19,12 @@ console.log(yas8)
 // Ahmet Güven 50 yaşın üstündeki ilk kişi olduğu için sonucu bulduktan sonra arama yapmayı bırakıyoruz. Arrayinizde ilk bulunan kişi sonucunuz olur. Yani herhangi bir isme göre sıralama vs. burada yoktur.
 let yas50 = kisiler.find( kisi => kisi.yas > 50 )
 console.log(yas50)
+
+// 5-) 100 yaşından büyük olanları bulalım
+let yas100 = kisiler.find( kisi => kisi.yas > 100 )
+console.log(yas100 || 'sonuç yok')
+// console.log(yas100 || sonucYok)
+
+// 6-) Kilosu 100'den fazla olanları bulalım
+let kilo100 = kisiler.find( kisi => kisi.kilo > 10 ) || "sonuç yok"
+console.log(kilo100)
